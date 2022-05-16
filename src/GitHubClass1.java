bd2348d76de702202138d5fc5b1b59421eb0803d
Kimia Bahrami <kimibhr@Kimias-MacBook.local>
2022-05-15 21:36:24 -0400

    changes after break

--- src/GitHubClass1.java
+++ src/GitHubClass1.java
@@ -4,6 +4,11 @@ public class GitHubClass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a =10;
+		int b=10;
+		int c =a+b;
+		System.out.println(c);
+				
 	}
 
 }
